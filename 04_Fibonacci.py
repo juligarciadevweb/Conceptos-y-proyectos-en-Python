0779df53ea6bc61a8eea999efb1e9b632d787a21
Julián García <118063845+juligarciadevweb@users.noreply.github.com>
2024-01-27 21:31:54 -0300

    Serie de Fibonacci
    Imprimir los primeros 50 números de la serie

---
+++ 04_Fibonacci.py
@@ -0,0 +1,21 @@
+"""
+* Escribe un programa que imprima los 50 primeros números de la sucesión
+ * de Fibonacci empezando en 0.
+ * - La serie Fibonacci se compone por una sucesión de números en
+ *   la que el siguiente siempre es la suma de los dos anteriores.
+ *   0, 1, 1, 2, 3, 5, 8, 13...
+"""
+
+def Fibonacci():
+	
+	prev = 0
+	next = 1
+
+	for index in range(0,51):
+		print(prev)
+		 
+		fib = prev + next
+		prev = next
+		next = fib
+
+Fibonacci()
